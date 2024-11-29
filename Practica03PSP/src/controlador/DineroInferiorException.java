11b097d39a4906fbf57da7a9422736c5a6b38453
aguepul054 <aguepul054@gmail.com>
2024-11-29 18:56:34 +0100

    Commit Nuevo

--- Practica03PSP/src/controlador/DineroInferiorException.java
+++ Practica03PSP/src/controlador/DineroInferiorException.java
@@ -4,6 +4,5 @@ public class DineroInferiorException extends Exception{
     public DineroInferiorException(){
         System.out.println("El saldo de la cuenta es inferior al saldo mínimo.");
     }
-
     
 }
